cabbf9d895113cdfee08aa0421ffcacdcc306a6a
Koushik Saha <93317898+Gituatu@users.noreply.github.com>
2023-10-04 03:49:09 +0530

    Add files via upload

---
+++ src/Components/Main.js
@@ -0,0 +1,44 @@
+import React from "react"
+import { Card } from "./Card"
+import axios from "axios"
+import { useState } from "react"
+import { useEffect } from "react"
+export const Main = () => {
+  const [url,setUrl]=useState("https://gateway.marvel.com/v1/public/characters?ts=1&apikey=425e2de99c8f78d84f3cf4318e5f9b34&hash=f2bfb67f8d2beff9790c5e6b8dc41bc2")
+  const [item,setItem]=useState();
+  const [search,setSearch]=useState("");
+  useEffect(()=>{
+    const fetch=async()=>{
+      const res=await axios.get(url)
+      setItem(res.data.data.results);
+    }
+    fetch();
+  },[url])
+  
+  const searchMarvel=()=>{
+    setUrl(`https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${search}&ts=1&apikey=425e2de99c8f78d84f3cf4318e5f9b34&hash=f2bfb67f8d2beff9790c5e6b8dc41bc2`)
+  }
+
+  return (
+    <>
+        <div className="header">
+            <div className="bg">
+                <img src="./Images/bg.png" alt="" />
+            </div>
+            <div className="search-bar">
+                <img src="./Images/logo.jpg" alt="logo" />
+                <input type="search" placeholder='Search Here'
+                 className='search'
+                 onChange={e=>setSearch(e.target.value)}
+                 onKeyUp={searchMarvel}/>
+            </div>
+        </div>
+       <div className="content">
+         
+        {
+          (!item)?<p>Not Found</p>:<Card data={item}/>
+        }
+       </div>
+    </>
+  )
+}
